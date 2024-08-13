fc10aaa6f60c2d368474a275e067af00e30bfe75
𝙄𝙣𝙘𝙤𝙣𝙣𝙪 𝙑𝟮 𝙈𝘿 <boyinconnu5@gmail.com>
2024-08-12 21:37:38 -0500

    Update pair.js

--- pair.js
+++ pair.js
@@ -58,7 +58,7 @@ router.get('/', async (req, res) => {
 
                let GIFTED_MD_TEXT = `
 ┏━━━━━━━━━━━━━━
-┃QUEEN_ANITA_V2 SESSION IS 
+┃X-V2-MD SESSION IS 
 ┃SUCCESSFULLY
 ┃CONNECTED ✅🔥
 ┗━━━━━━━━━━━━━━━
